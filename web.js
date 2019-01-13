b3fe1ec8cd132ce7b83ae5cfdc89a5ec5b139e37
Tom5aly <tom.aly@hotmail.com>
2019-01-13 22:45:22 +0000

    first commit

---
+++ web.js
@@ -0,0 +1,21 @@
+$("#button-a").click(function() {
+  swal({
+    title: "tom.aly@hotmail.com"
+  });
+});
+
+$(document).on("click", "#link", function(e) {
+  swal({
+    title: "Contact Me ",
+    confirmButtonText: "Send an Email",
+    showCancelButton: true
+  }).then(result => {
+    if (result.value) {
+      window.location = href = "mailto:tom.aly@hotmail.com";
+    } else if (result.dismiss === "cancel") {
+      swal("Maybe later ", "Thank you ");
+    }
+  });
+});
+
+
